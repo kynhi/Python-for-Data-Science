7afa09b1bb30e6a5d4c3335a1a3ac804f52dd4a4
Nhi Luong <nhi.luong@mavs.uta.edu>
2020-11-24 12:08:49 -0600

    Data Aggregation

--- Pandas_Structure.py
+++ Pandas_Structure.py
@@ -3,22 +3,25 @@ import numpy as np
 import pandas as pd
 
 heights_A = pd.Series([176.2,158.4,167.6,156.2,161.4],index = ['s1','s2','s3','s4','s5'])
-
-print(heights_A.shape)
 weights_A = pd.Series([85.1,90.2,76.8,80.4,78.9],index = ['s1','s2','s3','s4','s5'])
-
-print(weights_A.dtype)
-
 df_A = pd.DataFrame({'Student_height': heights_A,'Student_weight':weights_A })
-
-print(df_A.shape)
-
 np.random.seed(100)
 heights_B = pd.Series(np.random.normal(170.0,25,5),index = ['s1','s2','s3','s4','s5'])
 weights_B = pd.Series(np.random.normal(75.0,12,5),index = ['s1','s2','s3','s4','s5'])
+df_B = pd.DataFrame({'Student_height': heights_B,'Student_weight':weights_B })
+
+print(heights_A.shape)
+
+print(weights_A.dtype)
+
+
+
+print(df_A.shape)
+
+
+
 print(heights_B.mean())
 
-df_B = pd.DataFrame({'Student_height': heights_B,'Student_weight':weights_B })
 print(df_B.columns)
 
 p = pd.Panel({'ClassA' : df_A,
@@ -52,3 +55,48 @@ print(df_B3)
 df_B4 = pd.read_csv('classB', header = None,skiprows = 2)
 
 print(df_B4)
+
+#6 DATA AGGREGATION
+
+df_A_filter1 = df_A[df_A['Student_height'] > 160] & df_A[df_A['Student_weight'] > 80]
+
+print(df_A_filter1)
+
+
+df_A_filter2 = df_A.filter(regex='5$',axis = 0)
+print(df_A_filter2)
+
+df_A['Gender'] = ['M','F','M','M','F']
+df_groups =  df_A.groupby('Gender')
+
+print(df_groups.mean())
+
+#DATA MERGING 1
+
+df_A['Gender'] = ['M','F','M','M','F']
+
+s = pd.Series([165.4,82.7,'F'],index = ['Student_height','Student_weight','Gender'])
+s.name = 's6'
+
+df_AA = df_A.append(s)
+
+print(df_AA)
+df_B.index = ['s7','s8','s9','s10','s11']
+
+df_B['Gender'] = ['F','M','F','F','M']
+
+df = pd.concat([df_AA,df_B])
+
+print(df)
+
+#DATA MERGING 2
+nameid = pd.Series(range(101,111))
+name = pd.Series(['person' + str(i) for i in range(1,11)])
+
+master = pd.DataFrame({'nameid':nameid,'name':name})
+print(master)
+transaction = pd.DataFrame({'nameid':[108,108,108,103],'product':['iPhone','Nokia','Micromax','Vivo']})
+print(transaction)
+mdf = master.merge(transaction,on = 'nameid',how= 'inner')
+
+print(mdf)
