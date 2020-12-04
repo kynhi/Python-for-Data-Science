bae9b3505f04ad24297aa6fd4a78f3a6f7ab1122
Nhi Luong <nhi.luong@mavs.uta.edu>
2020-12-03 19:58:54 -0600

    Add Chapter 2 from Python for Data Science

--- Pandas_Structure.py
+++ Pandas_Structure.py
@@ -129,3 +129,74 @@ print(transaction)
 mdf = master.merge(transaction,on = 'nameid',how= 'inner')
 
 print(mdf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Write your code here
+
+import numpy as np
+import pandas as pd
+
+heights_A = pd.Series([176.2,158.4,167.6,156.2,161.4],index = ['s1','s2','s3','s4','s5'])
+weights_A = pd.Series([85.1,90.2,76.8,80.4,78.9],index = ['s1','s2','s3','s4','s5'])
+df_A = pd.DataFrame({'Student_height': heights_A,'Student_weight':weights_A })
+np.random.seed(100)
+heights_B = pd.Series(np.random.normal(170.0,25,5),index = ['s1','s2','s3','s4','s5'])
+weights_B = pd.Series(np.random.normal(75.0,12,5),index = ['s1','s2','s3','s4','s5'])
+df_B = pd.DataFrame({'Student_height': heights_B,'Student_weight':weights_B })
+
+
+df_A.to_csv('classA.csv')
+df_A2= pd.read_csv('classA.csv')
+
+print(df_A2)
+
+df_A3 = pd.read_csv('classA.csv',index_col=0)
+
+print(df_A3)
+
+df_B.to_csv('classB.csv')
+
+df_B2 = pd.read_csv('classB.csv')
+
+print(df_B2)
+
+df_B3 = pd.read_csv('classB.csv', header = None)
+
+print(df_B3)
+
+df_B4 = pd.read_csv('classB.csv', header = None,skiprows = 2)
+
+print(df_B4)
